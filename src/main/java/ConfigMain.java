d36990ba51773ff0e2fc18f1e9cba83105bddf1b
sdn <jim-jim0511@hanmail.net>
2016-08-23 13:52:53 +0900

    onos address fixed

--- src/main/java/ConfigMain.java
+++ src/main/java/ConfigMain.java
@@ -18,7 +18,7 @@ public class ConfigMain {
         OpenstackConfig openstackConfig = new OpenstackConfig();
         try {
             HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost postRequest = new HttpPost("http://tcp:192.168.20.5:8181/vnetmanager/openstacknode");
+            HttpPost postRequest = new HttpPost("http://192.168.20.5:8181/vnetmanager/openstacknode");
 
             postRequest.addHeader("accept", "application/json");
             JSONObject jsonObject = openstackConfig.readConfig();
